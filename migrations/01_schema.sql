a2effd08ed575c490cc15de61f939881ad868ed5
Clint Hiles <clinthiles1992@gmail.com>
2024-07-24 15:37:05 -0700

    creating tables for lightbnb added comments

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,6 +1,8 @@
 -- create tables for database
+-- drop existing tables if the exist to ensure clean slate
 DROP TABLE IF EXISTS users, reservations, property_reviews, properties CASCADE;
 
+-- stores info about registered users
 CREATE TABLE users (
   id       SERIAL PRIMARY KEY NOT NULL,
   name     VARCHAR(255) NOT NULL,
@@ -8,10 +10,12 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL
 );
 
+-- contains details about available rentals
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
+  -- property details
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
@@ -21,6 +25,7 @@ CREATE TABLE properties (
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms  INTEGER NOT NULL DEFAULT 0,
 
+  -- property locations
   country   VARCHAR(255) NOT NULL,
   street    VARCHAR(255) NOT NULL,
   city      VARCHAR(255) NOT NULL,
@@ -29,6 +34,7 @@ CREATE TABLE properties (
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
+ -- tracks bookings made by users
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
@@ -37,6 +43,7 @@ CREATE TABLE reservations (
   guest_id    INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
+  -- stores reviews left by users/guests
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id       INTEGER REFERENCES users(id)        ON DELETE CASCADE,
