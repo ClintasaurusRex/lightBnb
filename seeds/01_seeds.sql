1bb36002cf1a22adb224e83c25172eaa00c0f924
Clint Hiles <clinthiles1992@gmail.com>
2024-07-24 17:14:19 -0700

    adding insert seeds to migrations and comments

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,6 +7,7 @@ VALUES ('James Hetfield', 'Hetfield@metallica.com', '$2a$10$FB/BOAVhpuLvpOREQVmv
 ('Jon Bon Jovi', 'deadoralive@bonjovi.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Saul Hudson', 'slash@gunsnroses.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
+-- adding properties to the properties table
 INSERT INTO properties (
   owner_id,
   title, description, thumbnail_photo_url, cover_photo_url,
@@ -18,12 +19,14 @@ INSERT INTO properties (
   (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezu', 'Newfoundland And Labrador', 44583
   );
 
+-- inserting new resveration data
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 2, 3),
        ('2019-01-04', '2019-02-01', 2, 2),
        ('2023-10-01', '2023-10-14', 1, 3
 );
 
+-- inserting property reviews to table
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (3, 2, 1, 3, 'messages'),
        (2, 2, 2, 4, 'messages'),
