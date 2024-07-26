8f23bf03ff60b0a3bccda074dc5a033bc4af7b63
Clint Hiles <clinthiles1992@gmail.com>
2024-07-25 17:38:07 -0700

    refactored to meet requirments

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -127,43 +127,36 @@ LIMIT $2;`, [guest_id, limit])
 const getAllProperties = (options, limit = 10) => {
   const queryParams = [];
   let queryString = `
-  SELECT properties.*, AVG(property_review.rating) AS average_rating
+  SELECT properties.*, AVG(property_reviews.rating) AS average_rating
   FROM properties
   LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
-  let whereClauseAdded = false;
-
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length}`;
-    whereClauseAdded = true;
+    queryString += `WHERE city LIKE $${queryParams.length} `;
   }
 
   if (options.owner_id) {
     queryParams.push(options.owner_id);
-    if (whereClauseAdded) {
-      queryString += `AND owner_id = $${queryParams.length}`;
-      whereClauseAdded = true;
-    } else {
-      queryString += `WHERE owner_id $${queryParams.length}`;
-      whereClauseAdded = true;
-    }
+    queryString += queryParams.length > 1 ? `AND` : `WHERE`;
+    queryString += ` owner_id = $${queryParams.length} `;
   }
+
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
-    if (whereClauseAdded) {
-      queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
-    } else {
-      queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
-      whereClauseAdded = true;
-    }
+    queryString += queryParams.length > 2 ? `AND` : `WHERE`;
+    queryString += ` cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
   }
-  queryString += `GROUP BY properties.id`;
+
+  // Add comment explaining price conversion
+  // Prices are stored in cents in the database, so we multiply by 100 to convert dollars to cents
+
+  queryString += `GROUP BY properties.id `;
 
   if (options.minimum_rating) {
     queryParams.push(options.minimum_rating);
-    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
   }
 
   queryParams.push(limit);
@@ -171,11 +164,10 @@ const getAllProperties = (options, limit = 10) => {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-  console.log(queryString, queryParams);
 
-  return pool.query(queryString, queryParams)
-    .then((res) => res.rows);
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
 //This works because .then always returns a promise. Even though we wrote the line return result.rows (where result.rows is an array of objects), .then automatically places that value in a promise. .then returns a promise, which is returned as a result of the entire getAllProperties function.
 
 /**
