556feb0ac25f0d9896b81148e63012c80d58d682
Clint Hiles <clinthiles1992@gmail.com>
2024-07-25 17:48:44 -0700

    refactored to meet req again

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -124,51 +124,44 @@ LIMIT $2;`, [guest_id, limit])
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const addProperty = function(property) {
-  const queryString = `
-    INSERT INTO properties (
-      owner_id,
-      title,
-      description,
-      thumbnail_photo_url,
-      cover_photo_url,
-      cost_per_night,
-      street,
-      city,
-      province,
-      post_code,
-      country,
-      parking_spaces,
-      number_of_bathrooms,
-      number_of_bedrooms
-    )
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-    RETURNING *;
+const getAllProperties = (options, limit = 10) => {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  LEFT JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
   `;
 
-  const values = [
-    property.owner_id,
-    property.title,
-    property.description,
-    property.thumbnail_photo_url,
-    property.cover_photo_url,
-    property.cost_per_night * 100, // Convert to cents
-    property.street,
-    property.city,
-    property.province,
-    property.post_code,
-    property.country,
-    property.parking_spaces,
-    property.number_of_bathrooms,
-    property.number_of_bedrooms
-  ];
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
 
-  return pool.query(queryString, values)
-    .then(res => res.rows[0])
-    .catch(err => {
-      console.error('Error in addProperty', err.stack);
-      throw err;
-    });
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 //This works because .then always returns a promise. Even though we wrote the line return result.rows (where result.rows is an array of objects), .then automatically places that value in a promise. .then returns a promise, which is returned as a result of the entire getAllProperties function.
